476804245872b6436022cc00b19f889ecf1b781d
Kole <slobodan.kosanovic@gmail.com>
2018-04-22 13:26:35 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -8,16 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
         int[] myIntegers = getIntegers(5);
-        for (int i = 0;i<myIntegers.length;i++){
+        for (int i = 0; i < myIntegers.length; i++) {
             System.out.println("Element " + i + " typed value was " + myIntegers[i]);
         }
         System.out.println("Average is " + getAverage(myIntegers));
     }
 
-    public static int[] getIntegers (int number){
+    public static int[] getIntegers(int number) {
         System.out.println("Enter " + number + "integer values.\r");
         int[] values = new int[number];
-        for (int i = 0; i<values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             values[i] = scanner.nextInt();
         }
 
@@ -25,13 +25,13 @@ public class Main {
 
     }
 
-    public static double getAverage(int[] array){
+    public static double getAverage(int[] array) {
         int sum = 0;
-        for (int i=0; i<array.length;i++){
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
 
-        return (double) sum / (double)array.length;
+        return (double) sum / (double) array.length;
     }
 
 }
